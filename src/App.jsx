6c3af83e89d533af1e00bf916f7399c84703370b
idrissi2401 <idrissi24012002@gmail.com>
2025-10-13 12:30:02 +0200

    loginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { useEffect, useState } from 'react';
-import { fetchUsers } from './api/userApi';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { fetchUsers } from "./api/userApi";
+import LoginPage from "./pages/LoginPage";
 
-function App() {
+function UsersPage() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
@@ -19,6 +21,22 @@ function App() {
         ))}
       </ul>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <nav style={{ marginBottom: "20px" }}>
+        <Link to="/">Accueil</Link> |{" "}
+        <Link to="/login">Connexion</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<UsersPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import './LoginPage.css';
+
+export default function LoginPage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Email :", email);
+        console.log("Mot de passe :", password);
+    };
+
+    return (
+    <div className="login-container">
+        <div className="logo-container">
+        <img src="/logo.png" alt="Logo" className="logo" />
+      </div>
+      <h2>Connexion</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Email :</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Mot de passe :</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+        <button type="submit">Se connecter</button>
+      </form>
+    </div>
+  );
+}
